d253366840b13331a2737a33ae7caf8bab3ce177
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2023-10-07 20:07:56 +0100

    Added CsvMole.SourceGenerators.Tests.csproj

---
+++ CsvMole.SourceGenerators.Tests/Compilation/Models/TestCompilationModel.cs
@@ -0,0 +1,9 @@
+﻿using CsvMole.Abstractions.Attributes;
+
+namespace CsvMole.SourceGenerators.Tests.Compilation.Models;
+
+public class TestCompilationModel
+{
+    [CsvOrder(0)]
+    public string Id { get; set; } = null!;
+}
---
+++ CsvMole.SourceGenerators.Tests/Compilation/TestModelCompilationTests.cs
@@ -0,0 +1,24 @@
+﻿namespace CsvMole.SourceGenerators.Tests.Compilation;
+
+public class TestModelCompilationTests
+{
+    [Test]
+    public void TestCompilationModel_CompilesAndParses()
+    {
+        // Arrange
+        const string text = "Id\n1";
+        using var stringReader = new StringReader(text);
+
+        // Act
+        var result = CompilationParser.Parse(stringReader)
+            .ToList();
+
+        // Assert
+        CollectionAssert.AllItemsAreNotNull(result);
+        CollectionAssert.IsNotEmpty(result);
+        Assert.That(result, Has.Count.EqualTo(1));
+
+        var first = result[0];
+        Assert.That(first.Id, Is.EqualTo("1"));
+    }
+}
