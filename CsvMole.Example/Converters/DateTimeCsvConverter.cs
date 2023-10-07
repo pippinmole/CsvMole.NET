5b987321f2207efec6b38dda6f7bf42561575769
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2023-10-07 19:14:30 +0100

    Added DateTimeCsvConverter.cs

---
+++ CsvMole.Example/Converters/DateTimeCsvConverter.cs
@@ -0,0 +1,21 @@
+﻿using CsvMole.Abstractions.Converters;
+
+namespace CsvMole.Example.Converters;
+
+public class DateTimeCsvConverter : CsvConverterBase<DateTime?>
+{
+    public override DateTime? ConvertFromString(string value)
+    {
+        if ( DateTime.TryParse(value, out var result) )
+        {
+            return result;
+        }
+
+        return null;
+    }
+
+    public override string ConvertToString(DateTime? value)
+    {
+        return value.HasValue ? value.Value.ToString("yyyy-MM-dd") : string.Empty;
+    }
+}
