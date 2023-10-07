6c37e0e2c4480fb255d28a6fc991a46c8990a4be
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2023-10-07 22:35:08 +0100

    Cleaned up test folder structure

--- CsvMole.SourceGenerators.Tests/Compilation/TestModelCompilationTests.cs
+++ CsvMole.SourceGenerators.Tests/Compilation/TestModelCompilationTests.cs
@@ -1,11 +1,19 @@
-﻿using CsvMole.Abstractions.Options;
+﻿using CsvMole.Abstractions.Attributes;
+using CsvMole.Abstractions.Options;
+using CsvMole.SourceGenerators.Tests.Compilation.Models;
 
 namespace CsvMole.SourceGenerators.Tests.Compilation;
+
+[CsvParser]
+public static partial class CompilationParser
+{
+    public static partial IEnumerable<TestCompilationModel> Parse(StringReader stringReader, CsvOptions? options = null);
+}
 
 public class TestModelCompilationTests
 {
     [Test]
-    public void TestCompilationModel_CompilesAndParses()
+    public void CompilationParser_OnlyOnePartial_CompilesAndParses()
     {
         // Arrange
         const string text = "Id\n1";
