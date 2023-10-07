8f4749e2217202ddb025049916279481dab7a796
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2023-10-07 20:25:46 +0100

    Created ModelBuilderExtensions.cs and cleaned up CsvParserSourceGenerator.cs

--- CsvMole.SourceGenerators/CsvParserSourceGenerator.cs
+++ CsvMole.SourceGenerators/CsvParserSourceGenerator.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Immutable;
-using System.Text;
+﻿using System.Text;
 using CsvMole.Source.Builders;
+using CsvMole.Source.Extensions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
 
@@ -18,136 +18,30 @@ internal sealed class CsvParserIncrementalGenerator : IIncrementalGenerator
             static (_, _) => true,
             /* turn the attribute and node into a model containing what you need to generate source code */
             GetSemanticTargetForGeneration);
-        
+
         context.RegisterSourceOutput(classDeclarations, static (spc, source) => Execute(source, spc));
     }
 
     private static PartialDeclaration GetSemanticTargetForGeneration(GeneratorAttributeSyntaxContext context,
         CancellationToken token)
     {
-        var classSymbol = context.TargetSymbol as INamedTypeSymbol ??
-                          throw new Exception("Target symbol is null or not a class");
-        var methodModels = GetMethodDeclarationsFrom(classSymbol);
+        if ( context.TargetSymbol is not INamedTypeSymbol namedTypeSymbol )
+            throw new Exception("TargetSymbol is not INamedTypeSymbol");
+        
+        var methodModels = namedTypeSymbol.GetMethodDeclarations();
 
         return new PartialDeclaration(
-            classSymbol.ContainingNamespace.ToDisplayString(),
-            classSymbol.Name,
+            namedTypeSymbol.ContainingNamespace.ToDisplayString(),
+            namedTypeSymbol.Name,
             methodModels
         );
-    }
-
-    private static MethodDeclaration ToModel(IMethodSymbol methodSymbol)
-    {
-        // This should be StringReader. TODO: Check this
-        var parameterType = methodSymbol.Parameters[0].Type;
-
-        // Outer = IEnumerable<T>, Inner = T
-        var (outer, inner) = ExtractIEnumerableGenericType(methodSymbol);
-        
-        _ = inner ?? throw new Exception("Inner is null");
-        _ = outer ?? throw new Exception("Outer is null");
-
-        // Get the properties from the model which the Csv Parser will return
-        var properties = GetPropertiesFromClass(inner);
-        
-        return new MethodDeclaration(
-            methodSymbol.Name,
-            parameterType.ToString(),
-            outer.ToString(),
-            inner.ToString(),
-            properties
-        );
-    }
-
-    /// <summary>
-    /// Given an <see cref="ITypeSymbol"/>, return an <see cref="ImmutableArray{T}"/> of <see cref="PropertyDeclaration"/>
-    /// </summary>
-    /// <param name="classSymbol"></param>
-    /// <returns></returns>
-    private static ImmutableArray<PropertyDeclaration> GetPropertiesFromClass(ITypeSymbol classSymbol)
-    {
-        var properties = ExtractPropertiesFromType(classSymbol);
-        
-        if ( properties is null )
-            return ImmutableArray<PropertyDeclaration>.Empty;
-        
-        var result = new List<PropertyDeclaration>();
-        
-        foreach ( var property in properties )
-        {
-            var converter = GetConverterAttributeFrom(property);
-            var asModel = new PropertyDeclaration(
-                property.Name,
-                property.Type.ToString(),
-                converter
-            );
-
-            result.Add(asModel);
-        }
-
-        return result.ToImmutableArray();
-    }
-    
-    private static ImmutableArray<MethodDeclaration> GetMethodDeclarationsFrom(INamedTypeSymbol classSymbol)
-    {
-        return classSymbol.GetMembers()
-            .OfType<IMethodSymbol>()
-            .Where(x => x.DeclaredAccessibility == Accessibility.Public)
-            .Where(x => x.IsStatic)
-            .Select(ToModel)
-            .ToImmutableArray();
-    }
-
-    public static (ITypeSymbol? IEnumerableType, ITypeSymbol? InnerType) ExtractIEnumerableGenericType(
-        IMethodSymbol method)
-    {
-        var returnType = method.ReturnType;
-
-        if ( returnType is not INamedTypeSymbol { IsGenericType: true } namedTypeSymbol ||
-             namedTypeSymbol.OriginalDefinition.ToDisplayString() != "System.Collections.Generic.IEnumerable<T>" )
-            return (null, null);
-        
-        // Now, genericTypeSymbol represents the 'T' in 'IEnumerable<T>'
-        var genericTypeSymbol = namedTypeSymbol.TypeArguments[0];
-        return (returnType, genericTypeSymbol);
-    }
-
-    public static ImmutableArray<IPropertySymbol>? ExtractPropertiesFromType(ITypeSymbol? modelSymbol)
-    {
-        return modelSymbol?.GetMembers()
-            .OfType<IPropertySymbol>()
-            .Where(x => x.DeclaredAccessibility == Accessibility.Public)
-            .Where(x => x.IsStatic == false)
-            .ToImmutableArray();
-    }
-
-    private static ConverterDeclaration? GetConverterAttributeFrom(IPropertySymbol propertySymbol)
-    {
-        var attributes = propertySymbol.GetAttributes();
-        
-        foreach ( var attribute in attributes )
-        {
-            var attributeType = attribute.AttributeClass?.ToDisplayString();
-
-            if ( attributeType == "CsvMole.Abstractions.Attributes.CsvConverter" )
-            {
-                var converterType = attribute.ConstructorArguments[0].Value?.ToString();
-
-                if ( converterType is null )
-                    continue;
-
-                return new ConverterDeclaration(converterType);
-            }
-        }
-
-        return null;
     }
 
     public static void Execute(PartialDeclaration partialDeclaration, SourceProductionContext context)
     {
         var builder = new PartialBuilder(partialDeclaration);
         var result = builder.Build();
-        
+
         context.AddSource($"{partialDeclaration.ClassName}.g.cs", SourceText.From(result, Encoding.UTF8));
     }
 }
---
+++ CsvMole.SourceGenerators/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+
+namespace CsvMole.Source.Extensions;
+
+internal static class ModelBuilderExtensions
+{
+    public static ImmutableArray<MethodDeclaration> GetMethodDeclarations(this INamedTypeSymbol classSymbol)
+    {
+        return classSymbol.GetMembers()
+            .OfType<IMethodSymbol>()
+            .Where(x => x.DeclaredAccessibility == Accessibility.Public)
+            .Where(x => x.IsStatic)
+            .Select(ToModel)
+            .ToImmutableArray();
+    }
+    
+    private static MethodDeclaration ToModel(IMethodSymbol methodSymbol)
+    {
+        // This should be StringReader. TODO: Check this
+        var parameterType = methodSymbol.Parameters[0].Type;
+
+        // Outer = IEnumerable<T>, Inner = T
+        var (outer, inner) = ExtractIEnumerableGenericType(methodSymbol);
+        
+        _ = inner ?? throw new Exception("Inner is null");
+        _ = outer ?? throw new Exception("Outer is null");
+
+        // Get the properties from the model which the Csv Parser will return
+        var properties = GetPropertiesFromClass(inner);
+        
+        return new MethodDeclaration(
+            methodSymbol.Name,
+            parameterType.ToString(),
+            outer.ToString(),
+            inner.ToString(),
+            properties
+        );
+    }
+    
+    public static (ITypeSymbol? IEnumerableType, ITypeSymbol? InnerType) ExtractIEnumerableGenericType(
+        IMethodSymbol method)
+    {
+        var returnType = method.ReturnType;
+
+        if ( returnType is not INamedTypeSymbol { IsGenericType: true } namedTypeSymbol ||
+             namedTypeSymbol.OriginalDefinition.ToDisplayString() != "System.Collections.Generic.IEnumerable<T>" )
+            return (null, null);
+        
+        // Now, genericTypeSymbol represents the 'T' in 'IEnumerable<T>'
+        var genericTypeSymbol = namedTypeSymbol.TypeArguments[0];
+        return (returnType, genericTypeSymbol);
+    }
+    
+    private static ConverterDeclaration? GetConverterAttributeFrom(IPropertySymbol propertySymbol)
+    {
+        var attributes = propertySymbol.GetAttributes();
+        
+        foreach ( var attribute in attributes )
+        {
+            var attributeType = attribute.AttributeClass?.ToDisplayString();
+
+            if ( attributeType == "CsvMole.Abstractions.Attributes.CsvConverter" )
+            {
+                var converterType = attribute.ConstructorArguments[0].Value?.ToString();
+
+                if ( converterType is null )
+                    continue;
+
+                return new ConverterDeclaration(converterType);
+            }
+        }
+
+        return null;
+    }
+    
+    /// <summary>
+    /// Given an <see cref="ITypeSymbol"/>, return an <see cref="ImmutableArray{T}"/> of <see cref="PropertyDeclaration"/>
+    /// </summary>
+    /// <param name="classSymbol"></param>
+    /// <returns></returns>
+    private static ImmutableArray<PropertyDeclaration> GetPropertiesFromClass(ITypeSymbol classSymbol)
+    {
+        var properties = ExtractPropertiesFromType(classSymbol);
+        
+        if ( properties is null )
+            return ImmutableArray<PropertyDeclaration>.Empty;
+        
+        var result = new List<PropertyDeclaration>();
+        
+        foreach ( var property in properties )
+        {
+            var converter = GetConverterAttributeFrom(property);
+            var asModel = new PropertyDeclaration(
+                property.Name,
+                property.Type.ToString(),
+                converter
+            );
+
+            result.Add(asModel);
+        }
+
+        return result.ToImmutableArray();
+    }
+    
+    public static ImmutableArray<IPropertySymbol>? ExtractPropertiesFromType(ITypeSymbol? modelSymbol)
+    {
+        return modelSymbol?.GetMembers()
+            .OfType<IPropertySymbol>()
+            .Where(x => x.DeclaredAccessibility == Accessibility.Public)
+            .Where(x => x.IsStatic == false)
+            .ToImmutableArray();
+    }
+}
