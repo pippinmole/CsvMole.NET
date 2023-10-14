58c276162724a8a2f68be6164f8c3bba0d680548
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2023-10-14 18:40:21 +0100

    Improving code coverage

---
+++ CsvMole.SourceGenerators.Tests/CsvDelimiterAttributeTests.cs
@@ -0,0 +1,32 @@
+﻿using CsvMole.Abstractions.Attributes;
+
+namespace CsvMole.SourceGenerators.Tests;
+
+public class CsvDelimiterAttributeTests
+{
+    [Test]
+    public void CsvDelimiterAttribute_Constructor_WithPipeDelimiter_AssignsCorrectProperties()
+    {
+        // Arrange
+        const string delimiter = "|";
+
+        // Act
+        var attribute = new CsvDelimiterAttribute(delimiter);
+
+        // Assert
+        Assert.That(attribute.Delimiter, Is.EqualTo(delimiter));
+    }
+    
+    [Test]
+    public void CsvDelimiterAttribute_Constructor_WithCommaDelimiter_AssignsCorrectProperties()
+    {
+        // Arrange
+        const string delimiter = ",";
+
+        // Act
+        var attribute = new CsvDelimiterAttribute(delimiter);
+
+        // Assert
+        Assert.That(attribute.Delimiter, Is.EqualTo(delimiter));
+    }
+}
