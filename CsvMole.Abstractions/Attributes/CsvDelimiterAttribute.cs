0d1d7e23816784d66624f0b9ade9de03ea160bd9
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2023-10-14 18:28:49 +0100

    Added CsvDelimiterAttribute.cs with source-generated implementation

---
+++ CsvMole.Abstractions/Attributes/CsvDelimiterAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace CsvMole.Abstractions.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class CsvDelimiterAttribute(string delimiter) : Attribute
+{
+    public string Delimiter { get; } = delimiter;
+}
--- CsvMole.Example/Models/CustomModel.cs
+++ CsvMole.Example/Models/CustomModel.cs
@@ -3,11 +3,13 @@ using CsvMole.Abstractions.Converters;
 
 namespace CsvMole.Example.Models;
 
+[CsvDelimiter(",")]
 public class CustomModel
 {
     [CsvOrder(0)]
     public string Id { get; set; } = null!;
 
-    [CsvOrder(1),CsvConverter(typeof(CsvDateTimeConverter))]
+    [CsvOrder(1)]
+    [CsvConverter(typeof(CsvDateTimeConverter))]
     public DateTime? Date { get; set; }
 }
